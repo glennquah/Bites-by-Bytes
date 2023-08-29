dfb693b0687ea15f882eb2fc029eee2839944b0e
glenn2030 <quah.glenn@gmail.com>
2023-08-30 01:20:57 +0800

    Seperated into frontend and backend

--- frontend.py
+++ frontend.py
@@ -1,17 +1,21 @@
 from gettext import dpgettext
 from typing import Final
-from tables import Filters
+import typing
 from telegram import InlineKeyboardMarkup, Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes, MessageHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CallbackQueryHandler
 
+from backend import insert_meal, get_meal_logs, create_database
+
+# Initialize the database
+create_database()
 
 TOKEN: Final = '6666000684:AAG8VteJNEBthN266225DWEr9Hj6uM4Xo3Y'
 BOT_USERNAME: Final = '@BitesByBytes_Bot'
 
 
-# Commands
+# Frotnend Commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello, Welcome to BitesByBytes! Simply just add this telebot to a group chat with your loved ones for your logging to start')
 
@@ -49,22 +53,49 @@ async def handle_keyboard_selection(update: Update, context: ContextTypes.DEFAUL
         if query is not None:
             await query.answer('Invalid selection.')
 
+user_states = {}
 
 async def breakfast_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Insert Breakfast description and picture')
-    # You can add additional steps here, such as waiting for the description and picture
-    # and then processing them accordingly.
+    query = update.callback_query
+    query.answer()  # acknowledge button press
 
-    # For example, you can send a message asking for description:
-    await update.message.reply_text('Please enter a description for your breakfast:')
+    user_id = query.from_user.id
+    user_states[user_id] = {'step': 'photo'}
 
-    # You can then handle user input using a callback or by checking the next message received.
-    # Once you receive the description, you can ask for the picture.
+    await query.message.edit_text('You have selected Breakfast! Please send me a photo of your meal!')
 
-    # Similarly, ask for the picture:
-    await update.message.reply_text('Now, please send a picture of your breakfast:')
-    
-    # You can then handle the picture and description accordingly in the following messages.
+async def handle_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print('Handling photo...')
+    message = update.message
+    user_id = message.from_user.id
+
+    if user_id in user_states and user_states[user_id]['step'] == 'photo':
+        user_states[user_id]['step'] = 'description'
+        user_states[user_id]['photo'] = message.photo[-1].file_id  # Save the file_id of the last photo
+
+        await message.reply_text('Great! Now send me a brief description of your meal!')
+    else:
+        await message.reply_text("Please send a photo of your meal first.")
+
+async def handle_message(update: Update, context: typing.Any):
+    print('Handling message...')
+    message = update.message
+    user_id = message.from_user.id
+
+    if user_id in user_states and user_states[user_id]['step'] == 'description':
+        description = message.text
+
+        # Get the saved photo file_id from user_states
+        photo_file_id = user_states[user_id]['photo']
+
+        # Insert the meal information into the database
+        insert_meal(user_id, 'Breakfast', photo_file_id, description)
+
+        del user_states[user_id]
+
+        await message.reply_text("Thank you for logging your meal!")
+    else:
+        await message.reply_text("I'm not sure what you mean.")
 
 
 async def lunch_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -75,6 +106,13 @@ async def dinner_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def snack_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Contact me at @glennquahh for any help!')
+
+async def print_logs_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    meal_logs = get_meal_logs()
+
+    for log in meal_logs:
+        user_id, meal_type, meal_description = log
+        print(f"{user_id} - {meal_type}: {meal_description}")
 
 # Responses
 
@@ -123,11 +161,12 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('custom', custom_command))
     app.add_handler(CommandHandler('log', log_command))
-    app.add_handler(CommandHandler('breakfast', breakfast_command))
+    app.add_handler(CallbackQueryHandler(handle_keyboard_selection, pattern='^log_.*'))
+    app.add_handler(CommandHandler('printlogs', print_logs_command))
 
     # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
-    app.add_handler(CallbackQueryHandler(handle_keyboard_selection))
+    app.add_handler(MessageHandler(filters.ATTACHMENT, handle_photo))
+    app.add_handler(MessageHandler(filters.ALL, handle_message))
 
 
     # Errors
@@ -136,3 +175,5 @@ if __name__ == '__main__':
     # Polls the bot
     print('Polling...')
     app.run_polling(poll_interval=3)
+
+
